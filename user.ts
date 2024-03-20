9bff2ad14ea07fe604555ba1dfaeb31828f048cd
Adeel shekhani <adeel.shekhani@wiztech.pk>
2024-03-20 15:16:31 +0500

    delete user work

--- user.ts
+++ user.ts
@@ -1,14 +1,17 @@
-export class User{
-    
-    private _username : string;
-    public get username() : string {
-        return this._username;
-    }
-    public set username(v : string) {
-        this._username = v;
-    }
-    
-    findAllUsers(){
-        return [];
-    }
+export class User {
+  private _username: string;
+  public get username(): string {
+    return this._username;
+  }
+  public set username(v: string) {
+    this._username = v;
+  }
+
+  deleteUser(userId: number) {
+    return { message: "user " + userId + " deleted successfully" };
+  }
+
+  findAllUsers() {
+    return [];
+  }
 }
