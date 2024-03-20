7cff567d71a83eb3a812a85fb9ceb92b6e71b7dc
Adeel shekhani <adeel.shekhani@wiztech.pk>
2024-03-20 09:35:34 +0500

    add user class

---
+++ user.ts
@@ -0,0 +1,14 @@
+export class User{
+    
+    private _username : string;
+    public get username() : string {
+        return this._username;
+    }
+    public set username(v : string) {
+        this._username = v;
+    }
+    
+    findAllUsers(){
+        return [];
+    }
+}
